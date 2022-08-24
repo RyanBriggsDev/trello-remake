9e21f57054137e1c32ebc9b66db0502265f724f4
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-08-24 23:02:45 +0100

    access notes/boards data

--- src/pages/Boards.jsx
+++ src/pages/Boards.jsx
@@ -8,62 +8,60 @@ import { useNavigate } from "react-router-dom";
 function Boards() {
 
   const [user, error] = useAuthState(auth);
-  const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
-  const [name, setName] = useState();
   const [boards, setBoards] = useState(null);
-  const [notes, setNotes] = useState(null);
   const [userDocId, setUserDocId] = useState(null)
+
 
   const navigate = useNavigate();
 
   // FETCH USER
   const fetchData = async () => {
-    try {
-      // get user doc id
-      const userQ = query(collection(db, "users"),
-      where("uid", "==", user.uid)
-      )
-      const userDocSnap = await getDocs(userQ)
-      const data = []
-      userDocSnap.forEach((doc) => {
-        return data.push({
-          id: doc.id,
-          data: doc.data(),
+    if(user) { 
+      try {
+        // get user doc id
+        const userQ = query(collection(db, "users"),
+        where("uid", "==", user.uid)
+        )
+        const userDocSnap = await getDocs(userQ)
+        const data = []
+        userDocSnap.forEach((doc) => {
+          return data.push({
+            id: doc.id,
+            data: doc.data(),
+          })
         })
-      })
-      setUserDocId(data[0].id)
-      
-      // fetch boards/notes
-      if (userDocId !== null) {
+        setUserDocId(data[0].id)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+        // fetch boards/notes
+        if (userDocId !== null) {
           try {
             const q = query(collection(db, `users/${userDocId}/boards`), 
             )
             const docSnap = await getDocs(q)
-            let boards = []
-      
-            docSnap.forEach((doc) => {
-              return boards.push({
-                id: doc.id,
-                data: doc.data(),
+            let boardData = []
+            docSnap.forEach((boardDoc) => {
+              return boardData.push({
+                id: boardDoc.id,
+                data: boardDoc.data(),
               })
             })
-            setBoards(boards)
-            }
+            setLoading(false)
+            setBoards(boardData)
+          }
           catch (error) {
             console.log(error)
           }
-      
+        }
       }
-    } catch (error) {
-        console.log(error)
-    }
-  }
 
   useEffect(() => {
     fetchData()
-    setLoading(false)
-  }, [user, loading, data]);
+  }, [user, loading, userDocId])
 
   if (loading) {
     return <p>loading...</p>
@@ -74,7 +72,7 @@ function Boards() {
       <div>
         {boards.map((board, id) => (
           <div key={board.id}>
-            <h3>{board.data.boardName}.</h3>
+            <h3>{board.data.title}</h3>
             <p>{board.data.note1}</p>
             <p>{board.data.note2}</p>
           </div>
