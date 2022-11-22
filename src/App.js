47ef37750874118c21d62f4c969b92ee5ebe8662
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-11-22 14:50:39 +0000

    app restart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import Nav from "./components/Nav";
-import Footer from "./components/Footer";
+
 
 import { AuthProvider } from "./context/AuthContext";
 
 import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import PortfolioRedirect from "./redirects/PortfolioRedirect";
-import Boards from './pages/Boards'
-import PasswordReset from "./pages/PasswordReset";
-import Dashboard from "./pages/Dashboard";
-import CreateBoard from "./pages/CreateBoard";
 
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
@@ -22,22 +14,9 @@ function App() {
     <AuthProvider>
       <ToastContainer />
       <Router>
-        <div className="content-container">
-          <Nav />
-          <div className="container">
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/register' element={<Register />} />
-              <Route path='/portfolioRe' element={<PortfolioRedirect />} />
-              <Route path='/boards' element={<Boards />} />
-              <Route path='/password-reset' element={<PasswordReset />} />
-              <Route path='/create-board' element={<CreateBoard />} />
-              <Route path='/dashboard' element={<Dashboard />} />
-            </Routes>
-          </div>
-          <Footer />
-        </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
       </Router>
     </AuthProvider>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,9 @@
-import { Link } from "react-router-dom"
-import { useState, useEffect } from "react"
-import { auth } from "../firebase"
+
 
 function Home() {
-
   return (
-    <div>
-      <h1>Trello helps teams move work forward.</h1>
-      <p>Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with Trello.</p>
-    </div>
+    <div>Home</div>
   )
-
 }
 
 export default Home
