21b1f1a0b2b5e4560a94e153f51388bc2e995bc7
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-11-23 21:42:00 +0000

    hamburger menu state toggle

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 function Nav() {
+
+  const [navOpen, setNavOpen] = useState(false)
+
   return (
     <nav className="content">
         <div className="nav-left">
           <Link className="btn" to='/'>Home</Link>
         </div>
         <div className="nav-right">
-          <ul className="mobile-nav-menu">
-            <Link className="btn nav-btn mx_05 inline-block" to='/'><li>Login</li></Link>
-            <Link className="btn nav-btn mx_05 inline-block" to='/'><li>Contact</li></Link>
-            <Link className="btn nav-btn mx_05 inline-block" to='/'><li>About</li></Link>
+          <ul className={navOpen ? 'mobile-nav-menu' : ''}>
+            <Link className="btn nav-btn mx_05 inline-block" to='/' id={navOpen ? 'show' : ''}><li>Login</li></Link>
+            <Link className="btn nav-btn mx_05 inline-block" to='/' id={navOpen ? 'show' : ''}><li>Contact</li></Link>
+            <Link className="btn nav-btn mx_05 inline-block" to='/' id={navOpen ? 'show' : ''}><li>About</li></Link>
           </ul>
-          <div className="hamburger">
+          <div className="hamburger" onClick={() => setNavOpen(!navOpen)}>
             <div className="hamburger-line"></div>
             <div className="hamburger-line"></div>
             <div className="hamburger-line"></div>
