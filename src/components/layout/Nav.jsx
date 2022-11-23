85accd3da8f5b84e8578c2ce15be4f4efb221042
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-11-23 16:36:32 +0000

    nav layout + btn classes

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -1,9 +1,23 @@
-
+import { Link } from "react-router-dom"
 
 function Nav() {
   return (
     <nav className="content">
-        nav
+        <div className="nav-left">
+          <Link className="btn" to='/'>Home</Link>
+        </div>
+        <div className="nav-right">
+          <ul>
+            <Link className="btn nav-btn mx_05 inline-block" to='/'><li>Login</li></Link>
+            <Link className="btn nav-btn mx_05 inline-block" to='/'><li>Contact</li></Link>
+            <Link className="btn nav-btn mx_05 inline-block" to='/'><li>About</li></Link>
+          </ul>
+          <div className="hamburger">
+            <div className="hamburger-line"></div>
+            <div className="hamburger-line"></div>
+            <div className="hamburger-line"></div>
+          </div>
+        </div>
     </nav>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.css';
+import './styles/nav.css';
+import './styles/button.css';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
