1054dc1cc335402f6c308fa944a663cce39f2a52
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-11-25 10:51:54 +0000

    nav auth login/reg/logout

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+
+import { auth, logout } from '../../firebase'
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function Nav() {
 
   const [navOpen, setNavOpen] = useState(false)
+  const [user, loading, error] = useAuthState(auth);
+
+  useEffect(() => {
+  }, [user, loading]);
 
   return (
     <nav className="content">
@@ -12,9 +19,15 @@ function Nav() {
         </div>
         <div className="nav-right">
           <ul className={navOpen ? 'mobile-nav-menu' : ''}>
-            <Link className="btn nav-btn mx_05 inline-block" to='/auth/login' id={navOpen ? 'show' : ''}><li>Login</li></Link>
-            <Link className="btn nav-btn mx_05 inline-block" to='/auth/register' id={navOpen ? 'show' : ''}><li>Register</li></Link>
-            <Link className="btn nav-btn mx_05 inline-block" to='/' id={navOpen ? 'show' : ''}><li>About</li></Link>
+            {!user 
+            ?
+            <>
+              <Link className="btn nav-btn mx_05 inline-block" to='/auth/login' id={navOpen ? 'show' : ''}><li>Login</li></Link>
+              <Link className="btn nav-btn mx_05 inline-block" to='/auth/register' id={navOpen ? 'show' : ''}><li>Register</li></Link>
+            </>
+            : 
+              <li onClick={() => logout()} className="btn nav-btn mx_05 inline-block" id={navOpen ? 'show' : ''}>Logout</li>
+            }
           </ul>
           <div className={`hamburger ${navOpen ? 'open' : ''}`} onClick={() => setNavOpen(!navOpen)}>
             <div className="hamburger-line"></div>
