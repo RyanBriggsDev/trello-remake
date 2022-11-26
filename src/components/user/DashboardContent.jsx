c016102223c0f9f3f8141c2a00ac36be6e042c3a
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-11-26 18:06:24 +0000

    board preview css

--- src/components/user/DashboardContent.jsx
+++ src/components/user/DashboardContent.jsx
@@ -4,8 +4,14 @@ import BoardPreview from "./BoardPreview"
 function DashboardContent() {
   return (
     <div className='dash-content p-1'>
-        <h3>View Your Boards</h3>
-        <BoardPreview />
+        <h3 className="my_05">View Your Boards</h3>
+        <div className="grid-1-1-1 g-1 my_05">
+            <BoardPreview title='Work' desc='All work related jobs.' color='red'/>
+            <BoardPreview title='Home' desc='Jobs for home.' color='orangered'/>
+            <BoardPreview title='Career' desc='Stuff I need to learn.' color='orange'/>
+            <BoardPreview title='Finance' desc='Money notes.' color='green'/>
+            <BoardPreview title='Car' desc='Cool Cars.' color='teal'/>
+        </div>
     </div>
   )
 }
