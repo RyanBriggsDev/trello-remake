ed8bbcae78fbd34dd39e8780c9d4bf1ad7a8665c
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-11-29 14:39:35 +0000

    redesign modal to better reflect trello's

--- src/components/modals/VideoModal.jsx
+++ src/components/modals/VideoModal.jsx
@@ -1,6 +1,7 @@
 import FuncButton from "../pageStructure/FuncButton"
 import whitePlayIcon from '../../assets/icons/play-icon-white.png'
 import blackCloseWindow from '../../assets/icons/close-window-black.png'
+import whiteCloseWindow from '../../assets/icons/close-window-white-60.png'
 
 import { useState } from "react"
 
@@ -20,16 +21,10 @@ function VideoModal() {
         {!videoModalOpen ? '' : 
           <>
             <div className="video-modal-background" onClick={onClick}>
-              <div className="video-modal-container">
-                <div className="video-modal-content">
-                  <div className="modal-close">
-                    <img src={blackCloseWindow} alt="black cross, close window" onClick={onClick}/>
-                  </div>
-                  <div className="video-modal-video">
+              <div className="video-modal-content">
+                  <img src={whiteCloseWindow} alt="black cross, close window" onClick={onClick}/>
                   <iframe src="https://www.youtube.com/embed/l3F3l3psqXY"></iframe>
-                  </div>
                 </div>
-              </div>
             </div>
           </>
         }
