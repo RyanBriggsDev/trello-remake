a5f43429c34ef38047927640713dc6ee72c632c7
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-11-29 20:58:27 +0000

    useCaseCard component

---
+++ src/components/demos/UseCases.jsx
@@ -0,0 +1,16 @@
+import UseCaseCard from "../cards/UseCaseCard"
+
+function UseCases() {
+  return (
+    <div className="grid-1-1-1 g-1">
+        <UseCaseCard 
+          title=''
+          
+        />
+        <UseCaseCard />
+        <UseCaseCard />
+    </div>
+  )
+}
+
+export default UseCases
