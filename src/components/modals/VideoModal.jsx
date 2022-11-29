07eb45c9f595f17b3a7777e9210816a8997bfbf9
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-11-29 13:42:13 +0000

    added video modal

--- src/components/modals/VideoModal.jsx
+++ src/components/modals/VideoModal.jsx
@@ -1,20 +1,40 @@
 import FuncButton from "../pageStructure/FuncButton"
+import whitePlayIcon from '../../assets/icons/play-icon-white.png'
+import blackCloseWindow from '../../assets/icons/close-window-black.png'
 
-import { useState, useEffect } from "react"
+import { useState } from "react"
 
 function VideoModal() {
 
   const [videoModalOpen, setVideoModalOpen] = useState(false)
 
-  const onClick = () => {
-    console.log('egg');
-  }
+  const onClick = () => setVideoModalOpen(!videoModalOpen)
 
   return (
-    <>
-      <FuncButton btnText='Watch video' func={onClick}/>
-        {/* <button onClick={() => console.log('eggs')}>Watch Video</button> */}
-    </>
+    <div className="video-modal-component">
+      <div className="video-modal-link">
+        <FuncButton btnText='Watch video' func={onClick}/>
+        <img onClick={onClick} src={whitePlayIcon} alt="white play icon" />
+      </div>
+      <div className="video-modal">
+        {!videoModalOpen ? '' : 
+          <>
+            <div className="video-modal-background">
+              <div className="video-modal-container">
+                <div className="video-modal-content">
+                  <div className="modal-close">
+                    <img src={blackCloseWindow} alt="black cross, close window" />
+                  </div>
+                  <div className="video-modal-video">
+                  <iframe src="https://www.youtube.com/embed/l3F3l3psqXY"></iframe>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        }
+      </div>
+    </div>
   )
 }
 
