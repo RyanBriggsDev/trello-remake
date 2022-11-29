a42efb8799574a74bc02ef44651e0ff1fc6032cd
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-11-29 21:17:11 +0000

    useCaseCard imagae

--- src/components/demos/UseCases.jsx
+++ src/components/demos/UseCases.jsx
@@ -1,5 +1,11 @@
 import UseCaseCard from "../cards/UseCaseCard"
 
+import projectManagement from '../../assets/home/useCases/projectManagement.png'
+import Brainstorming from '../../assets/home/useCases/Brainstorming.png'
+import Meetings from '../../assets/home/useCases/Meetings.png'
+import CRM from '../../assets/home/useCases/CRM.png'
+import Calendar from '../../assets/home/useCases/Calendar.png'
+
 function UseCases() {
   return (
     <div className="grid-1-1-1 g-1">
@@ -7,16 +13,19 @@ function UseCases() {
           title='Project Management'
           color='rgb(255, 143, 115)'
           desc='Keep tasks in order, deadlines on track, and team members aligned with Trello.'
+          src={projectManagement}
         />
         <UseCaseCard 
           title='Brainstorming'
           color='rgb(121, 226, 242)'
           desc='Unleash your team’s creativity and keep ideas visible, collaborative, and actionable.'
+          src={Brainstorming}
         />
         <UseCaseCard 
           title='Meetings'
           color='rgb(121, 242, 192)'
           desc='Empower your team meetings to be more productive, empowering, and dare we say—fun.'
+          src={Meetings}
         />
         <UseCaseCard 
           title='Task Management'
@@ -27,11 +36,13 @@ function UseCases() {
           title='CRM'
           color='rgb(0, 199, 229)'
           desc={`Customize your CRM to help your sales team manage their leads and accounts effectively.`}
+          src={CRM}
         />
         <UseCaseCard 
           title='Editorial Calendar'
           color='rgb(135, 119, 217)'
           desc={`Use Trello as your team’s go-to command center for content curation, revisions, handoff, and publishing.`}
+          src={Calendar}
         />
     </div>
   )
