b59721c4f3a8fd9f500746f8639f87adcae463ec
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-12-02 13:45:32 +0000

    ways to grow section layout

--- src/components/demos/WaysToGrow.jsx
+++ src/components/demos/WaysToGrow.jsx
@@ -8,10 +8,10 @@ function WaysToGrow() {
             <h2 className="my-1 font-2_25">Do more with Trello</h2>
             <p className="font-1 w-75">Trello’s intuitive features give any team the ability to quickly set up and customize workflows for just about anything.</p>
         </div>
-        <div className="grid-1-1-1">
-            <Card jsx={<>Eggs</>}/>
-            <Card jsx={<>Eggs</>}/>
-            <Card jsx={<>Eggs</>}/>
+        <div className="grid-1-1-1 g-1_5">
+            <Card divClass='bg-lgray p-1-5 br' jsx={<>Eggs</>}/>
+            <Card divClass='bg-lgray p-1-5 br' jsx={<>Eggs</>}/>
+            <Card divClass='bg-lgray p-1-5 br' jsx={<>Eggs</>}/>
         </div>
     </div>
   )
