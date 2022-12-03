df777a4939b22eb2fd62861b1827768ad03cb918
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-12-03 11:45:48 +0000

    404 error page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,18 @@
+import Header from "../components/pageStructure/Header"
+
+function NotFound() {
+  return (
+    <div className="grid-2-1 g-1_5 not-found">
+        <Header 
+            title='404 Error'
+            titleClassName='font-5'
+            desc={`This isn't good. It looks like you're probably lost.`}
+            btnText={`Go drunk, you're home.`}
+            link='/'
+        />
+        <iframe src="https://media.tenor.com/xK2AXhBXoE4AAAAd/john-travolta-lost.gif" frameborder="0"></iframe>
+    </div>
+  )
+}
+
+export default NotFound
